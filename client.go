ea5fa643f78adf6b36a8b4161dcf65e1f8d86f8a
muye <xinqli@foxmail.com>
2022-06-12 22:40:33 +0800

    客户端连接

---
+++ client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial err:", err)
+		return nil
+	}
+	client.conn = conn
+	return client
+}
+
+func main() {
+	client := NewClient("127.0.0.1", 8888)
+	if client == nil {
+		fmt.Println(">>>>>服务端连接失败>>>>>")
+		return
+	}
+	fmt.Println(">>>>>服务端连接成功>>>>>")
+	// 启动客户端的业务
+	select {}
+}
